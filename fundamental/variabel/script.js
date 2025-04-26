bf2180728567b5d32c176132d157676f1dd5cc8e
Rafi <rafiisnanto131025@gmail.com>
2025-04-26 18:04:23 +0700

    belajar variabel

---
+++ fundamental/variabel/script.js
@@ -0,0 +1,47 @@
+// jadi ini adalah command
+
+// variabel dengan let
+
+let nama = "Rafi Isnanto";
+
+
+// tampilkan data
+
+console.log(nama);
+
+
+// kita ubah variabel nama
+
+nama = "Rafi Syahlefi";
+
+console.log(nama);
+
+// variabel dengan var
+
+var namaDepan = "Rafi";
+console.log(namaDepan);
+
+namaDepan = "Rafi Isnanto";
+console.log(namaDepan);
+
+// kelakukan dari let
+let namaBelakang = "Syahlefi";
+{
+    let namaBelakang = "Isnanto";
+    console.log(namaBelakang);
+
+}
+
+console.log(namaBelakang);
+
+// kelakuan dari var
+var namaTengah = "Oke";
+{
+    var namaTengah = "gg";
+    console.log(namaTengah);
+}
+console.log(namaTengah);
+
+// variabel dengan const
+const TTL = "10 Agustus 2000";
+console.log(TTL);
