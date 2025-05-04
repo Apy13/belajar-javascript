334638b04496cee936ffab6934df67d591291a1f
Rafi <rafiisnanto131025@gmail.com>
2025-05-04 12:57:52 +0700

    belajar boolean

---
+++ fundamental/tipe_data/boolean/script.js
@@ -0,0 +1,22 @@
+// tipe data boolean
+
+let data_boolean = false; //false = 0, true = 1
+console.log(data_boolean);
+
+let data_boolean2 = (3 < 10);
+console.log(data_boolean2);
+
+let data_boolean3 = ("ucup" == "otong");
+console.log(data_boolean3);
+
+let data_boolean4 = (true == 1);
+console.log(data_boolean4);
+
+let data_boolean5 = (false == 0);
+console.log(data_boolean5);
+
+data_boolean5 = (false == 0n);
+console.log(data_boolean5);
+
+data_boolean5 = (false == " ");
+console.log(data_boolean5);
