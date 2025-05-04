59bc41a76f317ff6628a0b0138589e184f415abf
Rafi <rafiisnanto131025@gmail.com>
2025-05-04 16:22:33 +0700

    Operasi Aritmatika

---
+++ fundamental/Operasi Aritmatika/script.js
@@ -0,0 +1,39 @@
+// operator aritmatika
+
+// ekspresi (expression) dan statement
+// % <- modulo
+// ++ <- increment
+// -- <- decrement
+// ** <- paangkat
+
+let x = 10;
+console.log(x + 1);
+console.log(x - 1);
+console.log(x / 4);
+console.log(x * 2);
+
+// aritmatika khusus di javascript
+
+console.log(x % 4); // sisa bagi
+console.log(x ** 2); // pangkat
+
+// pemograman biasa 
+x = x + 1;
+console.log(x);
+x = x + 1;
+console.log(x);
+x++;
+x++;
+console.log(x);
+
+x = x - 1;
+console.log(x);
+x--;
+console.log(x);
+
+// studi kasus
+
+let y = 1 + "1";
+console.log(y); // 11
+y = 1 + parseInt("1");
+console.log(y); // 2
