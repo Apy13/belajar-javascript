f0b81bfb31e4d150a4d5554a8796f1b2dcd065c3
Rafi <rafiisnanto131025@gmail.com>
2025-05-04 13:50:35 +0700

    tipe data special

---
+++ fundamental/tipe_data/tipe_data special/script.js
@@ -0,0 +1,17 @@
+// 1. NaN = not a Number
+
+console.log(`akar dari -1 :${Math.sqrt(-1)}`);
+console.log(`"ucup" / "otong" :${"ucup" / "otong"}`);
+console.log(`"ucup" * "otong" :${"ucup" *  "otong"}`);
+console.log(`"ucup" + "otong" :${"ucup" +  "otong"}`);
+console.log(`"ucup" - "otong" :${"ucup" -  "otong"}`);
+
+let data = parseInt("test123");
+console.log(data + 5);
+// naN ini tidak bisa dioperasikan lagi
+
+// 2. undefined - dia belum tau nilainya
+let a;
+console.log(`a : ${a}`);
+console.log(Math.sqrt(4));
+console.log(console.log("test"));
