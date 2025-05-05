f4b6007f3e8d6226c1a335e4d4c36923d0eb278e
Rafi <rafiisnanto131025@gmail.com>
2025-05-05 21:32:24 +0700

    Operator Komparasi

---
+++ fundamental/Operator Komparasi/script.js
@@ -0,0 +1,15 @@
+console.log(10 > 7);
+
+let dataBoolean = (10 > 7);
+console.log(dataBoolean);
+
+console.log(10 >= 10);
+console.log( 7 <= 7);
+
+// persamaan
+console.log("3" == 3);
+
+console.log("3" === 3);
+
+console.log(3 !== 3);
+console.log("3" !== 3);
