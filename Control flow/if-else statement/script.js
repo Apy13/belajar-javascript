67841df1575b02bd97226c9baa939978c319a0ed
Rafi <rafiisnanto131025@gmail.com>
2025-05-14 19:11:56 +0700

    Belajar if-else

---
+++ Control flow/if-else statement/script.js
@@ -0,0 +1,11 @@
+let nama = "ucup";
+
+console.log ("nama =  " +  nama);
+
+if(nama === "otong" ) {
+    console.log("hallo otong");
+}else {
+    console.log("au ah bukan otong");
+}
+
+console.log("akhir dari program");
